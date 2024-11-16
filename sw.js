4f59f68b3f40d48126b065e869cd199f30470802
Michael K-F <63935978+MichaelK-F@users.noreply.github.com>
2024-11-16 17:31:03 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,15 @@
+// Listen for request events
+self.addEventListener('fetch', function (event) {
+
+  // Get the request
+  let request = event.request;
+
+  // Bug fix
+  // https://stackoverflow.com/a/49719964
+  if (event.request.cache === 'only-if-cached' && event.request.mode !== 'same-origin') return;
+
+});
+
 // Send the request to the network first
 // If it's not found, look in the cache
 event.respondWith(
