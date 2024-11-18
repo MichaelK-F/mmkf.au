51b306d3e087290c6b315c0be24b07a22477e8bb
Michael K-F <63935978+MichaelK-F@users.noreply.github.com>
2024-11-18 09:23:39 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,23 +1,34 @@
-// Listen for request events
-self.addEventListener('fetch', function (event) {
+const CACHE_KEY = 'offline';
+const CACHE_FILE_URL = 'offline.html';
 
-  // Get the request
-  let request = event.request;
-
-  // Bug fix
-  // https://stackoverflow.com/a/49719964
-  if (event.request.cache === 'only-if-cached' && event.request.mode !== 'same-origin') return;
-
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_KEY).then(
+            (cache) => {
+                cache.addAll([CACHE_FILE_URL]).then(() => self.skipWaiting());
+            }
+        ).catch(err => console.log('Some error occurred while opening cache', err))
+    );
+    self.skipWaiting();
 });
 
-// Send the request to the network first
-// If it's not found, look in the cache
-event.respondWith(
-  fetch(request).then(function (response) {
-    return response;
-  }).catch(function (error) {
-    return caches.match(request).then(function (response) {
-      return response;
-    });
-  })
-);
+self.addEventListener("activate", event => {
+    event.waitUntil(
+      caches.keys().then(keyList => {
+        return Promise.all(
+          keyList.map((key) => {
+            if (key === CACHE_KEY) {
+              return;
+            }
+            return caches.delete(key);
+          }),
+        );
+      }),
+    );
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        fetch(event.request.url).catch(() => caches.match(CACHE_FILE_URL))
+    );
+});
