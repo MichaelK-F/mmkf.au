699d43e24f4199ded930e374263888d496c39b15
Michael K-F <63935978+MichaelK-F@users.noreply.github.com>
2024-11-18 09:37:53 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,9 +1,43 @@
-self.addEventListener('fetch', event => { 
-  if (event.request.method != 'GET') return;
-  event.respondWith(async function() {
-    const cache = await caches.open(CACHE_NAME);
-    const cached = await cache.match(event.request);
-    // If no cached version, fall back to server fetch
-    return cached ? cached : fetch(event.request);
-  })
-});
+const addResourcesToCache = async (resources) => {
+    const cache = await caches.open("v1");
+    await cache.addAll(resources);
+}
+
+const cacheMatch = async (request, preloadResponsePromise) => {
+    const cachedResponse = await caches.match(request)
+    if (cachedResponse) return cachedResponse
+    try {
+        const preloadResponse = await preloadResponsePromise;
+        if (preloadResponse) {
+            const cache = await caches.open("v1")
+            await cache.put(request, preloadResponse.clone())
+            return preloadResponse
+        }
+        const networkResponse = await fetch(request)
+        const cache = await caches.open("v1")
+        await cache.put(request, networkResponse.clone())
+        return networkResponse
+    } catch (err) {
+        return new Response("Response not found!")
+    }
+}
+
+self.addEventListener("install", (event) => {
+    self.skipWaiting()
+    event.waitUntil(addResourcesToCache([
+        "/",
+        "/index.html"
+    ]))
+})
+
+self.addEventListener("activate", event => {
+    event.waitUntil(clients.claim().then(() => console.log("SW has claimed all the clients")))
+    event.waitUntil(async () => {
+        if (self.registration.navigationPreload) {
+            await self.registration.navigationPreload.enable();
+        }
+    })
+})
+self.addEventListener("fetch", (event) => {
+    event.respondWith(cacheMatch(event.request, event.preloadResponse))
+})
