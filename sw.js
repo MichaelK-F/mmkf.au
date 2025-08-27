3d7c096aca4bf887bb95f29044ba4079cefad250
Michael K-F <michael@wandoo.ca>
2025-08-27 10:04:22 +0800

    updating the links

--- sw.js
+++ sw.js
@@ -34,7 +34,8 @@ self.addEventListener("install", (event) => {
     event.waitUntil(addResourcesToCache([
         "/",
         "/index.html",
-        "favicon.png"
+        "favicon.png",
+        "/*"
     ]));
 });
 
